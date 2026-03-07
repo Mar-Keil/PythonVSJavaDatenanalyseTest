ee9d96b66bb2349890f7b7664bcd94d302e14904
marcokeilwagen <marco.keilwagen@nordakademie.de>
2026-03-07 14:14:51 +0100

    feat: add groupCount_polars

---
+++ python/polars/groupCount_polars.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import polars as pl
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_GEN_OUT = BASE_DIR.parent.parent / "data-gen" / "out"
+FLIGHTS_PATH = DATA_GEN_OUT / "flights.parquet"
+AIRLINES_PATH = DATA_GEN_OUT / "airlines.parquet"
+OUT_DIR = BASE_DIR / "out"
+OUT_PATH = OUT_DIR / "groupCount.parquet"
+
+
+def load_datasets() -> tuple[pl.DataFrame, pl.DataFrame]:
+    flights = pl.read_parquet(FLIGHTS_PATH)
+    airlines = pl.read_parquet(AIRLINES_PATH)
+    return flights, airlines
+
+
+def groupCount_datasets(flights: pl.DataFrame) -> pl.DataFrame:
+    return flights.group_by("aircraft_model", "airline_code").agg(
+        pl.col("flight_distance").count().alias("count_aircraft")
+    )
+
+if __name__ == "__main__":
+    flights_df, airlines_df = load_datasets()
+    count_flights_df = groupCount_datasets(flights_df)
+    OUT_DIR.mkdir(parents=True, exist_ok=True)
+    count_flights_df.write_parquet(OUT_PATH, compression="zstd")
