300f33feaad3c8db811c51c8d83d61edf7d4c145
MK <marco.keilwagen@nordakademie.de>
2026-03-25 22:02:17 +0100

    refactor: refactor invocation_loop logic into its own class

--- python/polars_src/benchmarking/run/read_benchmark.py
+++ python/polars_src/benchmarking/run/read_benchmark.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from threading import Timer
-
 from polars_src.benchmarking.default.default_values import AIRLINES_INPUT_PATH
-from polars_src.benchmarking.default.default_values import BENCHMARK_DURATION_SECONDS
 from polars_src.benchmarking.default.default_values import BENCHMARK_ITERATIONS
 from polars_src.benchmarking.default.default_values import PARAM
+from polars_src.benchmarking.default.invocation_loop import InvocationLoop
 from polars_src.benchmarking.default.time_cpu_measurement import TimeCPUMeasurement
 from polars_src.logic.logic import read_parquet
 
@@ -14,18 +12,11 @@ class ReadBenchmark:
     def run(self, time_measurement: TimeCPUMeasurement) -> None:
         for path in PARAM:
 
-            for _ in range(BENCHMARK_ITERATIONS):
+            for i in range(BENCHMARK_ITERATIONS):
+                invocation_loop = InvocationLoop()
+                invocation_loop.start()
 
-                invocation_loop = True
-
-                def stop_invocation() -> None:
-                    nonlocal invocation_loop
-                    invocation_loop = False
-
-                timer = Timer(BENCHMARK_DURATION_SECONDS, stop_invocation)
-                timer.start()
-
-                while invocation_loop:
+                while invocation_loop.get_is_looping():
                     time_measurement.start()
 
                     flights = read_parquet(path)
@@ -36,6 +27,6 @@ class ReadBenchmark:
                     del flights
                     del airlines
 
-                timer.cancel()
+                invocation_loop.cancel()
 
             time_measurement.write_results(path.stem.replace("Flights", ""), "Read")
